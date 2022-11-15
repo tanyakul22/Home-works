47fe00a05b6822b0101ac8ce4cd184998d124e96
Tatyana Kulakova <117149308+tanyakul22@users.noreply.github.com>
2022-11-15 18:20:26 +1000

    Readme.md

---
+++ Exs001/Exs002/Program.cs
@@ -0,0 +1,19 @@
+Console.WriteLine("Hello, World!");
+
+ int num_1 =44;
+ int num_2 = 5;
+ int num_3 =78;
+ int max= num_1;
+
+
+            if (num_2 > max)
+            {
+                max = num_2;
+            }
+
+            if (num_3 > max)
+            {
+                max = num_3;
+            }
+
+            Console.WriteLine("Наибольшее из введённых чисел  " + max);
